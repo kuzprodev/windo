484fb7b1df82fa643184e9327f3a7b4b5339e105
kuzprodev <kuzprodev@gmail.com>
2023-08-03 13:08:50 +0300

    timer

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import forms from './modules/forms'
 import modals from './modules/modals'
 import tabs from './modules/tabs'
 import './slider'
+import timer from './modules/timer'
 
 window.addEventListener('DOMContentLoaded',()=>{
     'use strict'
@@ -11,12 +12,14 @@ window.addEventListener('DOMContentLoaded',()=>{
     //     type: "tree"
     // } //якщо юзер не зробив вибір то такі данні підуть у форму, якщо зробив то ті шо зробив
 let modalState = {}
+let deadline = '2024-02-01'
 
     changeModalState(modalState)
     modals();
     tabs('.glazing_slider','.glazing_block', '.glazing_content', 'active');
     tabs('.decoration_slider','.no_click', '.decoration_content > div > div', 'after_click');
     tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block')
-    forms(modalState)    
+    forms(modalState)  
+    timer('.container1', deadline)  
 })
 
